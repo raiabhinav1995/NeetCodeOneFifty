08007d0fa99cc9eb86827cec0dc836a6cf4081f2
Abhinav Rai <raiabhinav1995@gmail.com>
2023-03-11 19:26:40 +0530

    Added git ignore

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        ArrayProblems ap=new ArrayProblems();
-        int[] arr=new int[]{1,2,3,4,5};
-        System.out.println(ap.containsDuplicate(arr));
-    }
-}
