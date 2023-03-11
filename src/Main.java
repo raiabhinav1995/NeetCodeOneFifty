6e378517c97711e8c92bb97f27d6ba051905f2fe
Abhinav Rai <raiabhinav1995@gmail.com>
2023-03-11 19:23:00 +0530

    Alternate approach to solve contains duplicate.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         ArrayProblems ap=new ArrayProblems();
-        int[] arr=new int[]{1,2,3,4,5,5};
+        int[] arr=new int[]{1,2,3,4,5};
         System.out.println(ap.containsDuplicate(arr));
     }
 }
